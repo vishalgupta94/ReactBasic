16fe2c8387abc4d6bb53ce101975aaa3ab38c193
Vishal <vishal.gupta@appfire.com>
2023-10-15 16:10:53 +0530

    useReducerComplete

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,80 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { useState } from 'react';
+import { useReducer } from 'react';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+let INCREMENT = 'increment'
+let DECREMENT = 'decrement'
+let VALUETOADD = 'valueToAdd'
+let SUBMIT = 'submit'
 
+// type INCREMENT = 'increment'
+// type DECREMENT = 'decrement'
+// type VALUETOADD = 'valueToAdd'
+// type SUBMIT = 'submit'
+
+type Action = {
+  type: string,
+  payload?: number
+}
+const reducer = function (state: any, action: Action) {
+  if (action.type === INCREMENT) {
+    return {
+      ...state,
+      count: state.count + 1
+    }
+  }
+  else if (action.type === DECREMENT) {
+    return {
+      ...state,
+      count: state.count - 1
+    }
+  } else if (action.type === VALUETOADD) {
+    return {
+      ...state,
+      valueToAdd: action.payload ?? 0
+    }
+  } else {
+    return {
+      ...state,
+      count: state.count + state.valueToAdd,
+      valueToAdd: 0
+    }
+  }
+}
 const App = function () {
-  const [count, setCount] = useState<number>(0);
-  const [valueToAdd, setValueToAdd] = useState<number>(0);
+  // const [count, setCount] = useState<number>(0);
+  // const [valueToAdd, setValueToAdd] = useState<number>(0);
+  const [state, dispatch] = useReducer(reducer, {
+    count: 0,
+    valueToAdd: 0
+  })
   const increment = () => {
-    setCount(count + 1);
+    dispatch({
+      type: INCREMENT
+    })
   }
   const decrement = () => {
-    setCount(count - 1);
+    dispatch({
+      type: DECREMENT
+    })
   }
 
   const handleChange = (event: any) => {
-    setValueToAdd(parseInt(event.target.value) || 0)
+    let abc = parseInt(event.target.value)
+    dispatch({
+      type: VALUETOADD,
+      payload: abc || 0
+    })
   }
 
   const handleSubmit = (event: any) => {
     event.preventDefault();
-    setCount(count + valueToAdd)
-    setValueToAdd(0)
+    // setCount(count + valueToAdd)
+    dispatch({
+      type: SUBMIT
+    })
   }
 
   return <div style={{ border: '1px solid black', width: '440px', height: '560px', display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly' }}>
@@ -40,7 +92,7 @@ const App = function () {
     </div>
     <div style={{ border: '1px solid black', flex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
       <div style={{ border: '1px solid black', width: '200px', height: '50px', }}>
-        {count}
+        {state.count}
       </div>
     </div>
     <div style={{ border: '1px solid black', flex: 1, display: 'flex' }}>
@@ -53,7 +105,7 @@ const App = function () {
         </div>
 
         <div style={{ border: '1px solid black', flex: 2, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-          <input style={{ border: '1px solid black' }} type="number" value={valueToAdd} onChange={handleChange} />
+          <input style={{ border: '1px solid black' }} type="number" value={state.valueToAdd} onChange={handleChange} />
         </div>
       </form>
     </div>
